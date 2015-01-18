4d3e302c9ec528b6721be344ff68042ada22e1f3
Nick Wientge <nick@exygeninteractive.com>
2015-01-17 17:45:15 -0800

    allow configuring sidemenu sides

--- components/ionSideMenuContainer/ionSideMenuContainer.js
+++ components/ionSideMenuContainer/ionSideMenuContainer.js
@@ -2,14 +2,30 @@ IonSideMenu = {
   snapper: null
 };
 
+Template.ionSideMenuContainer.created = function () {
+  this.side = this.data.side || 'both';
+};
+
 Template.ionSideMenuContainer.rendered = function () {
   $snapperEl = this.$('.snap-content');
   if (!$snapperEl) {
     return;
   }
 
+  var disable;
+  if (this.side == 'both') {
+    disable = 'none';
+  }
+  if (this.side == 'left') {
+    disable = 'right';
+  }
+  if (this.side == 'right') {
+    disable = 'left';
+  }
+
   IonSideMenu.snapper = new Snap({
-    element: $snapperEl.get(0)
+    element: $snapperEl.get(0),
+    disable: disable
   });
 };
 
