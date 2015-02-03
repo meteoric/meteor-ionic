9e8c5e7cd59f471da436fb1fea34eac0fd4dc986
Nick Wientge <nick@exygeninteractive.com>
2015-02-03 12:32:22 -0800

    publish 0.1.13

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "meteoric:ionic",
   summary: "Ionic components for Meteor. No Angular!",
-  version: "0.1.12",
+  version: "0.1.13",
   git: "https://github.com/meteoric/meteor-ionic.git"
 });
 
