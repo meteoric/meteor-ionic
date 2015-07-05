8437b552d972729f5e85b83e6f2118876c88aec2
Elie Steinbock <eliesteinbock@gmail.com>
2015-07-05 15:30:36 +0300

    small fix to ionTab and version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "meteoric:ionic",
   summary: "Ionic components for Meteor. No Angular!",
-  version: "0.1.18",
+  version: "0.1.19",
   git: "https://github.com/meteoric/meteor-ionic.git"
 });
 
