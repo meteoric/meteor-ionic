72614deba02b2f988c0309496cfb44f3d299dfb6
woniesong92 <hs459@cornell.edu>
2015-08-13 23:05:23 -0700

    Fixed bug that when you open a newPost modal and tries to open a keyboard too fast, it goes away. Also, slighly modified keyboard animation duration

--- components/ionKeyboard/ionKeyboard.js
+++ components/ionKeyboard/ionKeyboard.js
@@ -43,8 +43,6 @@ IonKeyboard = {
 };
 
 window.addEventListener('native.keyboardshow', function (event) {
-  // return;
-
   // TODO: Android is having problems
   if (Platform.isAndroid()) {
     return;
@@ -54,68 +52,28 @@ window.addEventListener('native.keyboardshow', function (event) {
   var keyboardHeight = event.keyboardHeight;
   // event.stopPropagation();
 
-  // Attach any elements that want to be attached
-  // $('[data-keyboard-attach]').each(function (index, el) {
-  //   $(el).data('ionkeyboard.bottom', $(el).css('bottom'));
-    // if ($('.comment-item').length < 2) {
-    //   // $(el).css({bottom: keyboardHeight});
-    //   $('.content').css({bottom: keyboardHeight});
-    // }
-// });
-
   $('[data-keyboard-attach]').each(function (index, el) {
     $(el).velocity({
       bottom: keyboardHeight
     }, {
-      duration: 200
+      duration: 170
     });
   });
-
-
-  // $('.new-post-footer').each(function (index, el) {
-  //   $(el).data('ionkeyboard.bottom', $(el).css('bottom'));
-  //   $(el).css({bottom: keyboardHeight});
-  // });
-
-  // Move the bottom of the content area(s) above the top of the keyboard
-  // $('.content.overflow-scroll').each(function (index, el) {
-  //   $(el).data('ionkeyboard.bottom', $(el).css('bottom'));
-  //   $(el).css({bottom: keyboardHeight});
-  // });
-
-  // Scroll to the focused element
-  // scrollToFocusedElement(null, keyboardHeight);
-
 });
 
 window.addEventListener('native.keyboardhide', function (event) {
-  // return;
-
   // TODO: Android is having problems
   if (Platform.isAndroid()) {
     return;
   }
 
-  // $('input, textarea').blur();
   $('body').removeClass('keyboard-open');
-
-  // event.stopPropagation();
 
   $('[data-keyboard-attach]').each(function (index, el) {
     $(el).velocity({
       bottom: 0
     }, {
-      duration: 200
+      duration: 0
     });
   });
-
-  // Detach any elements that were attached
-  // $('[data-keyboard-attach]').each(function (index, el) {
-  //   $(el).css({bottom: $(el).data('ionkeyboard.bottom')});
-  // });
-
-  // $('.content.overflow-scroll').each(function (index, el) {
-  //   $(el).css({bottom: $(el).data('ionkeyboard.bottom')});
-  // });
-
 });
