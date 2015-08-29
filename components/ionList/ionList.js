f9571981f165e7951124377de2c6a64592912cdc
Jordan Davis <jdavis@nnapf.com>
2015-08-29 01:04:48 -0700

    minor code style

--- components/ionList/ionList.js
+++ components/ionList/ionList.js
@@ -1,10 +1,10 @@
 Template.ionList.helpers({
-  classes: function() {
+  classes: function () {
     var classes = ['list'];
 
     if (this.class) {
       var customClasses = this.class.split(' ');
-      _(customClasses).each(function(customClass) {
+      _(customClasses).each(function (customClass) {
         classes.push(customClass);
       });
     }
