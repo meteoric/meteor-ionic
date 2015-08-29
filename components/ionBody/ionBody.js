2dbf71fd84b6a0f79758213eb96c26516e854a55
Jordan Davis <jdavis@nnapf.com>
2015-08-29 00:37:29 -0700

    cleanup

--- components/ionBody/ionBody.js
+++ components/ionBody/ionBody.js
@@ -1,12 +1,12 @@
 Platform = {
   isIOS: function () {
     return (!!navigator.userAgent.match(/iPad/i) || !!navigator.userAgent.match(/iPhone/i) || !!navigator.userAgent.match(/iPod/i))
-    || Session.get('platformOverride') === 'iOS';
+           || Session.get('platformOverride') === 'iOS';
   },
 
   isAndroid: function () {
     return navigator.userAgent.indexOf('Android') > 0
-    || Session.get('platformOverride') === 'Android';
+           || Session.get('platformOverride') === 'Android';
   }
 };
 
