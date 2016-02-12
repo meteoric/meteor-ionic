27473db191267a3fffcf10d9a361808a5a470a9b
Joey Arnold <jandres@ualberta.ca>
2016-02-11 23:11:00 -0700

    bumped to 0.1.47-alpha2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "jandres:ionic",
   summary: "Ionic components for Meteor. No Angular!",
-  version: "0.1.47-alpha1",
+  version: "0.1.47-alpha2",
   git: "https://github.com/JoeyAndres/meteor-ionic.git"
 });
 
