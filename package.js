fb083cbc1ba74b5b6fca5aa239952e39e9b7d7ff
Joey Arnold <jandres@ualberta.ca>
2016-03-08 23:35:23 -0700

    bumped to 0.1.57

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "jandres:ionic",
   summary: "Ionic components for Meteor. No Angular!",
-  version: "0.1.56",
+  version: "0.1.57",
   git: "https://github.com/JoeyAndres/meteor-ionic.git"
 });
 
